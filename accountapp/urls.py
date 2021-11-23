14a6b3a0c9c8995ea37fe72ed8f7e59ce7e43be9
changlotech <changlotech@daum.net>
2021-11-23 17:40:53 +0900

    account유저생성 어드민

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "accountapp"
 
 urlpatterns = [
-    path('', views.hello_world)
+    path('', views.hello_world, name='hello_world'),
+    path('create/', views.AccountCreateView.as_view(), name='create'),
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,4 +1,19 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
 
 def hello_world(request):
     return render(request, 'accountapp/hello.html')
+
+class AccountCreateView(CreateView):
+    #유저라는 모델로 CreatView를 만들것이다.
+    model = User
+    #User 모델을 만들기위한 폼은 UserCreationForm을 이용할 것이다.
+    form_class = UserCreationForm
+    #계정생성을 성공하면 리다이렉트할 위치
+    success_url = reverse_lazy('accountapp:hello_world')
+    #UserCreationForm을 찍어낼 template 즉 html은 무엇을 사용할 것인가
+    template_name = 'accountapp/create.html'
