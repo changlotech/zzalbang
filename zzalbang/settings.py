55530b42634fe5735f8387b4be6ec5718895b7bd
changlotech <changlotech@daum.net>
2021-11-23 16:25:51 +0900

    html css 분리작업

--- zzalbang/settings.py
+++ zzalbang/settings.py
@@ -121,6 +121,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+#nginx 에서 스태틱 파일 관리. 쉽고 가볍게.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+#장고는 루트디렉토리의 스태틱폴더를 인식 못하므로, 인식하도록
+# 장고는 앱폴더 안에있는 templates 폴더만을 인식할수 있수 있어서 따로 루트폴더에
+# templates 폴더를 만드려면 따로 등록을 해줘야 했었던 것처럼
+# static 폴더 역시 앱폴더 안에 있는 static 폴더라고 인식을 한다.
+# 루트폴더에 static 폴더를 만들면 장고는 이 폴더가 static 폴더라고 인식을 못한다!
+#   그렇기 때문에 루트폴더에 static 폴더를 만들려면 따로 작업을 해주어야한다.
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
