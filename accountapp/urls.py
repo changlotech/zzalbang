e64a9abb1a233c7fe8cde2d212aab5befc59c0cb
changlotech <changlotech@daum.net>
2021-11-24 15:36:44 +0900

    로그인 로그아웃 버튼구현

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from . import views
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.hello_world, name='hello_world'),
     path('create/', views.AccountCreateView.as_view(), name='create'),
     path('login/', LoginView.as_view(template_name = 'accountapp/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name = 'logout'),
 
 ]
