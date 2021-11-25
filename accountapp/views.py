27fa5c608b5e7ea273507799202d3f2d2aa87aee
changlotech <changlotech@daum.net>
2021-11-25 16:43:08 +0900

    로그인 회원가입 내페이지 버튼

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView, UpdateView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from accountapp.forms import AccountUpdateForm
 
@@ -41,4 +41,14 @@ class AccountUpdateView(UpdateView):
     #정보수정을 할 때 html은 어떤 template html을 쓸 것인지
     template_name = 'accountapp/update.html'
     #위의 html에서 User모델을 어떤 변수로 화면출력이 되게 할 것인가
+    context_object_name = 'target_user'
+
+class AccountDeleteView(DeleteView):
+    #어떤 모델을 쓸건지
+    model = User
+    #회원 탈퇴가 성공하면 어디로 리다이렉트 할 것인지
+    Success_url = reverse_lazy('accountapp:login')
+    #회원탈퇴 할 때 쓸 html은 어떤 template html 이용할 것인지
+    template_name = 'accountapp/delete.html'
+    #위의 템플릿에서 User모델을 어떤 변수명으로 사용할 것인지
     context_object_name = 'target_user'
