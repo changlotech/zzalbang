132fa61a2f0805812e1d096d7af4a1098d0b508a
changlotech <changlotech@daum.net>
2021-12-01 16:34:35 +0900

    profile 업데이트 타인접근 방지

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -7,4 +7,5 @@ app_name= 'profileapp'
 #views의 함수와 url들 연결
 urlpatterns = [
     path('create/', views.ProfileCreateView.as_view(), name='create'),
+    path('update/<int:pk>/', views.ProfileUpdateView.as_view(), name='update'),
 ]
--- profileapp/views.py
+++ profileapp/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, UpdateView
 
+from profileapp.decorator import profile_ownership_required
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
 
@@ -27,3 +29,17 @@ class ProfileCreateView(CreateView):
         temp_profile.save()
         #자바의 @오버라이드 대신하는 줄
         return super().form_valid(form)
+
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
+class ProfileUpdateView(UpdateView):
+    # 어떤 모델을 쓸 것인가.
+    model = Profile
+    # Profile 모델을 수정하기 위한 폼은 ProfileCreationForm이용할 것이다.
+    form_class = ProfileCreationForm
+    # Profile 정보를 수정을 성공하면 리다이렉트할 위치
+    success_url = reverse_lazy('accountapp:hello_world')
+    #Profile 정보를 수정할 때 쓸 html은 어떤 template html쓸 것인가
+    template_name = 'profileapp/update.html'
+    # Profile 객체를 html(템플릿파일)에서 찍어낼 때 어떤 변수로 찍어낼 것인가
+    context_object_name = 'target_profile'
